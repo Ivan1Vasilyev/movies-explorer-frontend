a4ecb52333274082b54be3f183cc10c5b3a81ae0
Ivan Vasilyev <Nalivan3747@yandex.ru>
2023-02-03 12:10:57 +0300

    feat: Preloader добавлен

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -3,14 +3,27 @@ import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Preloader from '../Preloader/Preloader';
+import { useEffect, useState } from 'react';
 
 const Movies = ({ moviesData, isButtonNeed, isOwner, place }) => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+
   return (
     <>
       <Header loggedIn={true} place={place || 'movies'} />
       <main className="movies page__element">
         <SearchForm />
-        <MoviesCardList moviesData={moviesData} isButtonNeed={isButtonNeed} isOwner={isOwner} />
+        {loading ? (
+          <Preloader />
+        ) : (
+          <MoviesCardList moviesData={moviesData} isButtonNeed={isButtonNeed} isOwner={isOwner} />
+        )}
       </main>
       <Footer />
     </>
